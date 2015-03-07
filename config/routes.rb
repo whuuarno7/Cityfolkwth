8029211402f11b19a389524e2f3cb7f0ac4dec07
whuuarno7 <jdguarnizom@unal.edu.co>
2015-03-07 12:18:35 -0500

    devise ok-notificaciones en español

--- config/routes.rb
+++ config/routes.rb
@@ -6,9 +6,12 @@ Rails.application.routes.draw do
 
   get 'events/index'
 
-  resources :events
-
+  devise_scope :user do
+    get 'users/sign_out' => "devise/sessions#destroy"
+end
   devise_for :users
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
