6ef5ba16a0053dc2aed18c20114c75b870cf6276
Luis Guillermo Solorzano Useche <MEMOSOLORZANO@Luiss-MacBook-Pro.local>
2015-03-14 22:09:53 -0500

    guille

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,8 @@ Rails.application.routes.draw do
 end
   devise_for :users
 
+  get "profile_user/:id" => "users_profiles#profile_user"
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
