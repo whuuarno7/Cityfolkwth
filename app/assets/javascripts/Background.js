6d0b4806433ccdb11a18fc6ac4e1808b531e0c6c
whuuarno7 <jdguarnizom@unal.edu.co>
2015-03-16 21:47:27 -0500

    events index con background y columnas 50%

--- app/assets/javascripts/Background.js
+++ app/assets/javascripts/Background.js
@@ -1,7 +1,20 @@
 $(document).ready(function(){
 	var value = $('#backgrounde').text();
-	$console.log(value);
+	/*console.log(value);*/
 	$('#julie').css('background', 'url('+value+')');
 	
 });
+
+$(document).ready(function(){
+	$( ".backgroundei" ).each(function(  ) {
+		var value = $(this).text();
+     $(this).parent(".background_imagediv").css('background-image', 'url('+value+')');
+    });
+
+
+	
+});
+
+
+
  
--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,5 +15,4 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require bootstrap-sprockets
-
-//removi el require tree por que me presentaba problemas
+//= require_tree 
